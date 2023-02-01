a224a392fe14e0001e3e7d50c0a1fc39340ad5b8
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-01 16:59:31 +0100

    completamento calcolo e stampaggio bilietto

--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
 //1 inserire in un input km e con la finestra selezionare il tipo di sconto
-const name = prompt('inserisci il tuo nome');
-const surname = prompt('inserisci il tuo cognome');
-const personalAge = prompt('inserisci la tua età');
-const totalKilometers = prompt('inserisci la quantita di km che intendi percorrere');
-console.log(name);
-console.log(surname);
-console.log(personalAge);
-console.log(totalKilometers);
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const personalAge = document.getElementById('reduction');
+const totalKilometers = document.getElementById('giveKilometers');
 
 //2 calcolare costo al click
-let ticketCost = (totalKilometers * 0.21).toFixed(2);
-console.log(ticketCost);
+let submit = document.querySelector('#submit');
+submit.addEventListener('click', function(){
+    let takeName = name.value;
+    let takeSurname = surname.value;
+    let takePersonalAge = personalAge.value;
+    let takeTotalKilometers = totalKilometers.value;
+    console.log(takeName);
+    console.log(takeSurname);
+    console.log(takePersonalAge);
+    console.log(takeTotalKilometers);
 
-//3 calcolare sconti
-let discountAmmount, ticketDiscounted;
-
-if (personalAge < 18){
-    discountAmmount = (ticketCost * 0.20).toFixed(2);
-    ticketDiscounted = ticketCost - discountAmmount;
-    console.log(discountAmmount);
-    console.log(ticketDiscounted);
-} else if (personalAge > 65){
-    discountAmmount = (ticketCost * 0.40).toFixed(2);
-    ticketDiscounted = ticketCost - discountAmmount;
-    console.log(discountAmmount);
-    console.log(ticketDiscounted);
-} else{
-    discountAmmount = 0
-    ticketDiscounted = ticketCost
-    console.log(discountAmmount);
+    let ticketCost = (takeTotalKilometers * 0.21).toFixed(2);
     console.log(ticketCost);
-}
-
-//4 stampare tutto a video
-const nameSurname = document.getElementById('nameSurname');
-const kilometers = document.getElementById('kilometers');
-const price = document.getElementById('price');
-const discount = document.getElementById('discount');
-const finalPrice = document.getElementById('finalPrice');
-
-nameSurname.innerText = `${name} ${surname}`;
-kilometers.innerText = `${totalKilometers}km`;
-price.innerText = `${ticketCost}$`;
-discount.innerText = `${discountAmmount}$`;
-finalPrice.innerText = `${ticketDiscounted}$`;
+    
+    //3 calcolare sconti
+    let discountAmmount, ticketDiscounted;
+    
+    if (takePersonalAge < 18){
+        discountAmmount = (ticketCost * 0.20).toFixed(2);
+        ticketDiscounted = (ticketCost - discountAmmount).toFixed(2);
+        console.log(discountAmmount);
+        console.log(ticketDiscounted);
+    } else if (takePersonalAge > 65){
+        discountAmmount = (ticketCost * 0.40).toFixed(2);
+        ticketDiscounted = (ticketCost - discountAmmount).toFixed(2);
+        console.log(discountAmmount);
+        console.log(ticketDiscounted);
+    } else{
+        discountAmmount = 0
+        ticketDiscounted = ticketCost
+        console.log(discountAmmount);
+        console.log(ticketCost);
+    }
+    
+    //4 stampare tutto a video
+    const nameSurname = document.getElementById('nameSurname');
+    const kilometers = document.getElementById('kilometers');
+    const price = document.getElementById('price');
+    const discount = document.getElementById('discount');
+    const finalPrice = document.getElementById('finalPrice');
+    
+    nameSurname.innerText = `${takeName} ${takeSurname}`;
+    kilometers.innerText = `${takeTotalKilometers}km`;
+    price.innerText = `${ticketCost}$`;
+    discount.innerText = `${discountAmmount}$`;
+    finalPrice.innerText = `${ticketDiscounted}$`;   
+    
+});
